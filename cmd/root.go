9fad077f962ef12d5bc1fa34044fe5744a22ffb4
Darren Hobbs <darren.hobbs@gmail.com>
2018-01-02 17:08:07 +0000

    Fix license summary

--- cmd/root.go
+++ cmd/root.go
@@ -1,4 +1,5 @@
-// Copyright © 2017 Twisted Systems Ltd
+// Lazenby - Your secrets as a service
+// Copyright © 2018 Twisted Systems Ltd
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -11,7 +12,7 @@
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with this program. If not, see <http://www.gnu.org/licenses/>.
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 package cmd
 
